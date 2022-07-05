90711f390a943fc92faf9ce4f77d1597ddcd1d3e
Marouane Ahal <d144013152@gmail.com>
2022-07-05 20:53:17 +0100

    pointcut expressions - match any addAccount method

--- src/com/luv2code/aopdemo/MainDemoApp.java
+++ src/com/luv2code/aopdemo/MainDemoApp.java
@@ -1,6 +1,7 @@
 package com.luv2code.aopdemo;
 
 import com.luv2code.aopdemo.dao.AccountDAO;
+import com.luv2code.aopdemo.dao.MembershipDAO;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MainDemoApp {
@@ -14,14 +15,14 @@ public class MainDemoApp {
         // get the bean from spring container
         AccountDAO theAccountDAO = context.getBean("accountDAO", AccountDAO.class);
 
+        // get membership bean from spring container
+        MembershipDAO theMembershipDAO = context.getBean("membershipDAO", MembershipDAO.class);
+
         // call the business method
         theAccountDAO.addAccount();
 
-        // do it again!
-        System.out.println("\n let's call it again!\n");
-
-        // call the business method again
-        theAccountDAO.addAccount();
+        // class the membership business method
+        theMembershipDAO.addAccount();
 
         // close the context
         context.close();
