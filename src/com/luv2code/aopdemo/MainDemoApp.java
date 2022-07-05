3b4a7b13ba7ca8349bade6cff19bbdd3555e5473
Marouane Ahal <d144013152@gmail.com>
2022-07-05 19:29:10 +0100

    AOP:@Before Advice

---
+++ src/com/luv2code/aopdemo/MainDemoApp.java
@@ -0,0 +1,25 @@
+package com.luv2code.aopdemo;
+
+import com.luv2code.aopdemo.dao.AccountDAO;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class MainDemoApp {
+
+    public static void main(String[] args) {
+
+        // read spring config java class
+        AnnotationConfigApplicationContext context =
+                new AnnotationConfigApplicationContext(DemoConfig.class);
+
+        // get the bean from spring container
+        AccountDAO theAccountDAO = context.getBean("accountDAO", AccountDAO.class);
+
+        // call the business method
+        theAccountDAO.addAccount();
+
+        // close the context
+        context.close();
+
+    }
+
+}
