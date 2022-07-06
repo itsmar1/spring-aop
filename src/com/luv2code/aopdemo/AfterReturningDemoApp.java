ade0fee07983956f09303e3feb974dae800d8750
Marouane Ahal <d144013152@gmail.com>
2022-07-06 21:56:28 +0100

    modify AccountDAO to simulate throwing an exception

--- src/com/luv2code/aopdemo/AfterReturningDemoApp.java
+++ src/com/luv2code/aopdemo/AfterReturningDemoApp.java
@@ -1,7 +1,6 @@
 package com.luv2code.aopdemo;
 
 import com.luv2code.aopdemo.dao.AccountDAO;
-import com.luv2code.aopdemo.dao.MembershipDAO;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import java.util.List;
@@ -18,7 +17,7 @@ public class AfterReturningDemoApp {
         AccountDAO theAccountDAO = context.getBean("accountDAO", AccountDAO.class);
 
         // call method to find the accounts
-        List<Account> theAccounts = theAccountDAO.findAccounts();
+        List<Account> theAccounts = theAccountDAO.findAccounts(false);
 
         // display the accounts
         System.out.println("\n\nMain Program: AfterReturningDemoApp");
